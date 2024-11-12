e667e245c47fae6ab0a4698ee54d5e3c50fd1485
Ava-TU <C24384981@myTUDublin.ie>
2024-11-12 21:09:35 +0000

    Q WORKS BUT CANT JUMP
    Pressing Q changes the gravity but cannot jump when upside down

--- gamejammovementtest1/Player.gd
+++ gamejammovementtest1/Player.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
+enum GRAV_DIR {
+	DOWN,
+}
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var grav = 1
 
+var grav_dir = GRAV_DIR.DOWN
 
 func _physics_process(delta: float) -> void:	
 	# Add the gravity.
@@ -17,6 +22,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("SWITCH"):
 		#Changes Gravity
 		grav = -grav
+		match grav_dir:
+			GRAV_DIR.DOWN:
+				up_direction = Vector2.UP
+		velocity += -up_direction * grav
+			
 		
 		
 	# Get the input direction and handle the movement/deceleration.
