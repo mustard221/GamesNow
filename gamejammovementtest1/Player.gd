200945a6892ff7729ced34f16c75cabf75328a5c
Ava-TU <C24384981@myTUDublin.ie>
2024-11-12 20:43:33 +0000

    Changes gravity but can't jump when upside down
    Cant jump when upside down and can only change the gravity when you jump and then press Q

--- gamejammovementtest1/Player.gd
+++ gamejammovementtest1/Player.gd
@@ -1,23 +1,32 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var grav = 1
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta: float) -> void:	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta*grav
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("JUMP") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if Input.is_action_just_pressed("SWITCH"):
+		#Changes Gravity
+		grav = -grav
+		
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("LEFT", "RIGHT")
 	if direction:
+		if direction > 0:
+			$Sprite2D.scale.x = 0.2
+		else:
+			$Sprite2D.scale.x = -0.2
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
